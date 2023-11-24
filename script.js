d99bfe44a45919e5667e449781c4ff07d7f0ba46
woshou <woshou@UKR.NET>
2023-11-24 13:12:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://localhost:5001/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
